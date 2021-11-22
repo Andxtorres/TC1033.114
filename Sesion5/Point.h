31572f9ba2b708dca9f6b202cb56467d297d96c7
Andres Torres <atorres@applab.mx>
2021-11-22 13:29:47 -0600

    Points

---
+++ Sesion5/Point.h
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "math.h"
+using namespace std;
+
+class Point{
+    private: 
+        double x;
+        double y;
+    public:
+        Point();
+        Point(double x, double y);
+        double getX();
+        double getY();
+        void setX(double x);
+        void setY(double y);
+        double calculateDistance(Point point2);
+};
+
+Point::Point(){
+    x=0;
+    y=0;
+}
+Point::Point(double x,double y){
+    this->x=x;
+    this->y=y;
+}
+
+double Point::getX(){
+    return x;
+}
+double Point::getY(){
+    return y;
+}
+
+void Point::setX(double x){
+    this->x=x;
+}
+void Point::setY(double y){
+    this->y=y;
+}
+
+double Point::calculateDistance(Point point2){
+    double distancia=0;
+    distancia=sqrt(pow((x-point2.getX()),2)+pow((y-point2.getY()),2));
+    return distancia;
+}
---
+++ Sesion5/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Point.h"
+using namespace std;
+
+int main(){
+
+    Point punto1;
+
+    cout<<"Las coordenadas del punto 1 son X: "<<punto1.getX()<< " Y: "<<punto1.getY()<<endl;
+
+    Point punto2(10,20);
+
+    cout<<"Las coordenadas del punto 2 son X: "<<punto2.getX()<< " Y: "<<punto2.getY()<<endl;
+
+    cout<<"La distancia entre punto 1 y punto 2 es: "<<punto1.calculateDistance(punto2)<<endl;
+    return 0;
+}
